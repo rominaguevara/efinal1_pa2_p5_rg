ede207491823d3f47ed09704863aebbec1dde88d
ec98 <carlospiruch98@gmail.com>
2024-01-15 18:23:15 -0500

    a

---
+++ src/main/java/com/uce/ec/Efinal1Pa2P5RgApplication.java
@@ -0,0 +1,21 @@
+package com.uce.ec;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Efinal1Pa2P5RgApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Efinal1Pa2P5RgApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		//15/01/2024
+		
+	}
+
+}
---
+++ src/test/java/com/uce/ec/Efinal1Pa2P5RgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Efinal1Pa2P5RgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
